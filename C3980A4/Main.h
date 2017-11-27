e43880e6d722df2777899b9d17e50e8785d7124a
MatthewShew <shewmj@gmail.com>
2017-11-27 09:46:04 -0800

    setup window and menu

--- C3980A4/Main.h
+++ C3980A4/Main.h
@@ -5,3 +5,10 @@
 #define TRANSMIT_TIMEOUT	2000
 #define RECEIVE_TIMEOUT		2000
 #define TEST_TIMEOUT		1000
+
+#define MENU_CONNECT			103
+#define MENU_DISCONNECT			104
+#define MENU_QUIT				105
+
+#define windowHeight			600
+#define windowWidth				400
