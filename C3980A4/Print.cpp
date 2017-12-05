1eaf7065752f930d80a8df2080a17582703084ea
MatthewShew <shewmj@gmail.com>
2017-12-04 17:13:37 -0800

    printing has proper spacing

--- C3980A4/Print.cpp
+++ C3980A4/Print.cpp
@@ -11,22 +11,59 @@ void print() {
 	   
 	TEXTMETRIC tm;
 	RECT rect;
-	//int windowWidth;
+	int currentWindowWidth;
 	int charHeight;
+	SIZE length;
 	
 
 	GetTextMetrics(dc, &tm);
 	charHeight = tm.tmExternalLeading + tm.tmHeight;
 
 	GetWindowRect(hwnd, &rect);
-	//windowWidth = rect.right - rect.left;
+	currentWindowWidth = rect.right - rect.left;
 
+
+
+	/*
+	
+	char remember[150];
+	int characters = 0;
+	int charHeight, rows = 0;
+	DWORD btransferred;
+	TEXTMETRIC tm;
+	SIZE length;
+	RECT rect;
+	int width;
+
+	GetTextMetrics(dc, &tm);
+	charHeight = tm.tmExternalLeading + tm.tmHeight;
+
+	GetWindowRect((HWND)n, &rect);
+	width = rect.right - rect.left;
+
+	while (connectMode) {
+		GetTextExtentPoint32(dc, remember, characters, &length);
+		if (length.cx >= width - 25) {
+			rows++;
+			memset(remember, ' ', characters);
+			characters = 0;
+
+			GetTextExtentPoint32(dc, remember, characters, &length);
+		}
+		if (ReadFile(port, buffer, sizeof(buffer), &btransferred, NULL)) {
+			if (btransferred) {
+				remember[characters] = buffer[0];
+				characters++;
+				sprintf_s(toScreen, "%c", buffer[0]);
+				TextOut(dc, length.cx, charHeight * rows, toScreen, sizeof(toScreen));
+			}
+		}
+	}*/
 
 
 	int nullCount = 0;
 	char buff[2];
-	//strncpy_s(temp, inputBuffer, sizeof(temp));
-
+	
 
 	for (int i = 2; i < 514; i++) {
 		if (inputBuffer[i] == NULL) {
@@ -35,22 +72,24 @@ void print() {
 		else 
 		{
 			sprintf_s(buff, "%c", '\0');
+			GetTextExtentPoint32(dc, buff, 1, &length);
 			while (nullCount > 0) {
 				if (printColumn > windowWidth) {
 					printColumn = 0;
-					printRow += 16;
+					printRow += charHeight;
 				}
 				TextOut(dc, printColumn, printRow, buff, sizeof(buff));
-				printColumn += 10;
+				printColumn += length.cx;
 				nullCount--;
 			}
 			if (printColumn > windowWidth - 25) {
 				printColumn = 0;
-				printRow += 16;
+				printRow += charHeight;
 			}
 			sprintf_s(buff, "%c", inputBuffer[i]);
+			GetTextExtentPoint32(dc, buff, 1, &length);
 			TextOut(dc, printColumn, printRow, buff, sizeof(buff));
-			printColumn += 10;
+			printColumn += length.cx;
 		}
 	}
 	ReleaseDC(hwnd, dc);
